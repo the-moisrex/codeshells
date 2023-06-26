b9719a7ec3ac2e23b9f38b0449de641a2030dd4f
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-06-26 11:44:26 -0800

    using isatty

--- c.is-stdout-piped/main.c
+++ c.is-stdout-piped/main.c
@@ -1,19 +1,25 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 int main() {
   struct stat stats;
-  if (fstat(0, &stats) == -1) {
+  int const fd = fileno(stdout);
+  printf("fd: %d\n", fd);
+  if (fstat(fd, &stats) == -1) {
     perror("fstat");
     exit(0);
   }
 
-  if (S_ISFIFO(stats.st_mode)) {
+  if (isatty(fd) && errno == 0) {
+    printf("It's a Terminal.\n");
+  } else if (S_ISFIFO(stats.st_mode)) {
     printf("Yes it is piped.\n");
   } else {
-    printf("No it's not piped.\n");
+    printf("It's a redirect.\n");
   }
   return 0;
 }
