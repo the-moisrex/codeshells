1cf77bdc899665904c8e40086ff6e515170b5474
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-01-16 10:00:50 -0800

    to lower update

--- cpp.to_lower/main.cpp
+++ cpp.to_lower/main.cpp
@@ -4,11 +4,35 @@ using namespace std;
 
 auto main() -> int {
 
-    char v = 'A';
-    char c = ((v + (128 - 'A')) ^ (v + (128 - 'Z' - 1)) & 0x80) >> 2;
-    v ^= c;
-    cout << static_cast<int>(c) << endl;
-    cout << v << endl;
+  // good article explaining this:
+  // http://0x80.pl/notesen/2023-03-06-swar-find-any.html
+  //
+  // Soruce:
+  // https://godbolt.org/z/qdnvTP3s3
 
-    return 0;
+  static constexpr char Ap = (128 - 'A');
+  static constexpr char Zp = (128 - 'Z' - 1);
+
+  cout << "Ap: " << static_cast<int>(Ap) << endl;
+  cout << "Zp: " << static_cast<int>(Zp) << endl;
+
+  char v = 'A';
+
+  cout << "v: " << static_cast<int>(v) << endl;
+  cout << "v + Ap: " << static_cast<int>((v + Ap)) << endl;
+  cout << "v + Zp: " << static_cast<int>((v + Zp)) << endl;
+  cout << "^: " << static_cast<int>((v + Ap) ^ (v + Zp)) << endl;
+  cout << "& 0x80: " << static_cast<int>(((v + Ap) ^ (v + Zp)) & 0x80) << endl;
+  cout << ">> 2: " << static_cast<int>((((v + Ap) ^ (v + Zp)) & 0x80) >> 2)
+       << endl;
+
+  char c = (((v + Ap) ^ (v + Zp)) & 0x80) >> 2;
+  cout << "v(int): " << static_cast<int>(v) << endl;
+  v ^= c;
+  cout << static_cast<int>(c) << " "
+       << "0x" << std::hex << static_cast<int>(c) << endl;
+  cout << v << endl;
+  cout << "v(int): " << std::dec << static_cast<int>(v) << " "
+       << "0x" << std::hex << static_cast<int>(v) << endl;
+  return 0;
 }
