2e6707c3888a158ec692887462a8ea674230d941
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-12-28 01:53:57 -1000

    update benchmark

--- cpp.benchmark.bool/main.cpp
+++ cpp.benchmark.bool/main.cpp
@@ -35,6 +35,31 @@ int test3(char32_t cu2, char32_t code_point) {
     return 2;
 }
 
+int test4(char32_t cu2, char32_t code_point, char32_t cu3) {
+                                code_point |= (cu2 & 0b0000'1111) << 12U;
+                            code_point |= (cu3 & 0b0011'1111) << 6U;
+    bool error = false;
+    error |= (cu2 & 0b1111'0000) != 0b1110'0000;
+    error |= code_point < 0x800;
+    error |= 0xFFFF < code_point;
+    error |= (0xD7FF < code_point && code_point < 0xE000);
+    if (error) [[unlikely]] {
+        return 1;
+    }
+    return 2;
+}
+
+int test5(char32_t cu2, char32_t code_point, char32_t cu3) {
+                                code_point |= (cu2 & 0b0000'1111) << 12U;
+                            code_point |= (cu3 & 0b0011'1111) << 6U;
+    if ((cu2 & 0b1111'0000) != 0b1110'0000 || code_point < 0x800 ||
+        0xFFFF < code_point || (0xD7FF < code_point && code_point < 0xE000))
+        [[unlikely]] {
+            return 1;
+    }
+    return 2;
+}
+
 
 static void One(benchmark::State& state) {
   for (auto _ : state) {
@@ -61,4 +86,21 @@ static void Three(benchmark::State& state) {
   }
 }
 BENCHMARK(Three);
+
+static void Four(benchmark::State& state) {
+  for (auto _ : state) {
+    auto res = test4(1, 2, 3);
+    benchmark::DoNotOptimize(res);
+  }
+}
+BENCHMARK(Four);
+
+
+static void Five(benchmark::State& state) {
+  for (auto _ : state) {
+    auto res = test5(1, 2, 3);
+    benchmark::DoNotOptimize(res);
+  }
+}
+BENCHMARK(Five);
 
