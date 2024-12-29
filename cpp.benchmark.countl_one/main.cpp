c0f6de3591a48296673798bdb5dc37608f237ae7
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-12-29 02:50:48 -1000

    countl_one vs table lookup

---
+++ cpp.benchmark.countl_one/main.cpp
@@ -0,0 +1,39 @@
+#include <benchmark/benchmark.h>
+
+#include <bit>
+
+template <typename CharT = char8_t>
+static constexpr CharT utf8_skip[256] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+    4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 1, 1};
+
+int ccl_mine(unsigned char a) { return utf8_skip<unsigned char>[a]; }
+
+int ccl(unsigned char a) { return std::countl_one(a); }
+
+volatile unsigned char num = 0b1100'0000;
+
+static void CountLeft(benchmark::State &state) {
+  for (auto _ : state) {
+    auto val = ccl(num);
+    benchmark::DoNotOptimize(val);
+  }
+}
+BENCHMARK(CountLeft);
+
+static void Table(benchmark::State &state) {
+  for (auto _ : state) {
+    auto val = ccl_mine(num);
+    benchmark::DoNotOptimize(val);
+  }
+}
+BENCHMARK(Table);
