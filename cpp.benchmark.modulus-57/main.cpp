2885fa6f8730cef30f8d47f30321c7860786a147
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2025-05-08 18:04:30 -1000

    benchmark

---
+++ cpp.benchmark.modulus-57/main.cpp
@@ -0,0 +1,48 @@
+#include <benchmark/benchmark.h>
+
+using namespace std;
+
+static void shift(benchmark::State &state) {
+  char32_t val;
+  for (auto _ : state) {
+    val >>= 10;
+    benchmark::DoNotOptimize(val);
+  }
+}
+BENCHMARK(shift);
+
+static void v57(benchmark::State &state) {
+  char32_t val;
+  for (auto _ : state) {
+    val %= 57;
+    benchmark::DoNotOptimize(val);
+  }
+}
+BENCHMARK(v57);
+
+static void v57045(benchmark::State &state) {
+  char32_t val;
+  for (auto _ : state) {
+    val %= 57045;
+    benchmark::DoNotOptimize(val);
+  }
+}
+BENCHMARK(v57045);
+
+static void v2(benchmark::State &state) {
+  char32_t val;
+  for (auto _ : state) {
+    val %= 2;
+    benchmark::DoNotOptimize(val);
+  }
+}
+BENCHMARK(v2);
+
+static void v60(benchmark::State &state) {
+  char32_t val;
+  for (auto _ : state) {
+    val %= 60;
+    benchmark::DoNotOptimize(val);
+  }
+}
+BENCHMARK(v60);
